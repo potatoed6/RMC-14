7cd5ceeb54bf04ec49bb2a0cc529503a5a91ecd7
potatoed6 <136747954+potatoed6@users.noreply.github.com>
2025-06-09 16:49:53 +0200

    Make default channel usable

--- Content.Shared/Chat/SharedChatSystem.cs
+++ Content.Shared/Chat/SharedChatSystem.cs
@@ -26,7 +26,7 @@ public abstract class SharedChatSystem : EntitySystem
     public const char AdminPrefix = ']';
     public const char WhisperPrefix = ',';
     public const char MentorPrefix = '}';
-    public const char DefaultChannelKey = 'h';
+    public const char DefaultChannelKey = ':';
 
     [ValidatePrototypeId<RadioChannelPrototype>]
     public const string CommonChannel = "MarineCommon";
